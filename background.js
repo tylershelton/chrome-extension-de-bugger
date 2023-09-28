c8517b24f380e2f5a6b0acb77bd8b21bb6b6b8ee
Tyler Shelton <tdshelton@gmail.com>
2023-09-28 00:32:06 -0700

    toggle button text and injected css on and off

--- background.js
+++ background.js
@@ -11,13 +11,32 @@ chrome.runtime.onInstalled.addListener(() => {
 // listen for enable/disable messages
 // from the extension. Start and stop
 // the game accordingly
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['game/game.js'],
+chrome.action.onClicked.addListener(async (tab) => {
+  // set the action badge to the next state
+  const prevState = await chrome.action.getBadgeText({tabId: tab.id});
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
   });
-  chrome.scripting.insertCSS({
-    target: {tabId: tab.id},
-    files: ['game/game.css'],
-  });
+
+  if (nextState === 'ON') {
+    // inject game files
+    await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['game/game.js'],
+    });
+    await chrome.scripting.insertCSS({
+      target: {tabId: tab.id},
+      files: ['game/game.css'],
+    });
+  }
+
+  if (nextState === 'OFF') {
+    await chrome.scripting.removeCSS({
+      target: {tabId: tab.id},
+      files: ['game/game.css'],
+    });
+  }
 });
