4739b1e176dcdc4b0c4c025a5e5c0a82392c143d
Tyler Shelton <tdshelton@gmail.com>
2023-09-28 00:15:18 -0700

    wip popup logic. currently unused

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,38 @@
 /* eslint-env browser */
+/* eslint-env webextensions */
+
 // logic for extension pop-up menu
-
-
 function handleEnableSwitch (e) {
   e.preventDefault();
-    
-  const label = document.querySelector('label');
-  // toggle label on the button (and the label next next to the button)
-  // when clicked
-  if (e.submitter.value === 'Off') {
-    e.submitter.value = 'On';
-    label.innerText = 'Disable De-Bugger';
-  }
-  else {
-    e.submitter.value = 'Off';
-    label.innerText = 'Enable De-Bugger';
-  }
+  
+  getCurrentTab().then(tab => {
+    const label = document.querySelector('label');
+    const button = e.submitter;
+  
+    // toggle label on the button (and the label next next to the button)
+    // when clicked
+  
+    if (button.value === 'Disable') {
+      button.value = 'Enable';
+      label.innerText = 'Enable De-Bugger';
+    }
+    else {
+      button.value = 'Disable';
+      label.innerText = 'Disable De-Bugger';
+    }
+  });
 }
 
+
+async function getCurrentTab() {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
   form.addEventListener('submit', handleEnableSwitch);
+
 });
